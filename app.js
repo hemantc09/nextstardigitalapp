72b8ea97e2748d7db52536902e25f44bb5fd335f
hemantc09 <hemantc09@gmail.com>
2019-09-10 23:13:30 -0700

    update server 3000

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = express();
 var request = require("request");
 
 //can create different ports, for different envs
-var port = 8080;
+// var port = 8080;
+
+const http = require('http');
+const port = process.env.PORT || 3000
+
 
 //App logic 
 app.set("view engine","ejs");
@@ -12,7 +16,7 @@ app.use(express.static(__dirname + "/public"));
 console.log(__dirname);
 
 //results route
-app.get("/",function(req,res){
+app.get("/results",function(req,res){
 
     //reqest API
     var url = "https://gist.githubusercontent.com/scottburton11/66a921c458f9500a773a6b0ac65006df/raw/629bfd6a3125e3428bd85a53231bd8018c407a65/Javascript%2520Working%2520With%2520Data%2520Challenge%2520data";
@@ -33,4 +37,8 @@ app.get("/",function(req,res){
 });
 
 //listening on 8080 port
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
